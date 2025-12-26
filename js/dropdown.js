fa52f2d3be21d07878b17babcc8a91197b398ca6
mne <moses.epps@gmail.com>
2025-12-25 23:56:12 -0800

    nav dropdown of services & location

---
+++ js/dropdown.js
@@ -0,0 +1,19 @@
+/* */ // delay the dropdown
+document.querySelectorAll('.dropdown').forEach((dropdown) => {
+  let closeTimer
+
+  const open = () => {
+    clearTimeout(closeTimer)
+    dropdown.classList.add('open')
+  }
+
+  const closeWithDelay = () => {
+    clearTimeout(closeTimer)
+    closeTimer = setTimeout(() => {
+      dropdown.classList.remove('open')
+    }, 800)
+  }
+
+  dropdown.addEventListener('mouseenter', open)
+  dropdown.addEventListener('mouseleave', closeWithDelay)
+})
